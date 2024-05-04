62bb296e59fd2bee9ad8bc26b3d932cd3e46bfb2
AlejandroNaranjoUdeA <alejandro.naranjo3@udea.edu.co>
2024-05-04 11:56:54 -0500

    Se agregan cambios en la implementacion de la redmetro

--- redmetro.cpp
+++ redmetro.cpp
@@ -1,6 +1,42 @@
 #include "redmetro.h"
 
-redMetro::redMetro()
+RedMetro::RedMetro()
 {
+    cantidadLineas = 0;
+    lineas = new Linea*[50]; // Capacidad inicial, puedes ajustarla según necesites
+}
 
+void RedMetro::agregarLinea(Linea* linea) {
+    if (cantidadLineas < 50) {
+        lineas[cantidadLineas] = linea;
+        cantidadLineas++;
+    } else {
+        cout << "Error: Capacidad máxima alcanzada." << endl;
+    }
+}
+
+void RedMetro::eliminarLinea(string nombre) {
+    for (int i = 0; i < cantidadLineas; i++) {
+        if (lineas[i]->getNombreLinea() == nombre) {
+            delete lineas[i];
+            for (int j = i; j < cantidadLineas - 1; j++) {
+                lineas[j] = lineas[j + 1];
+            }
+            cantidadLineas--;
+            return;
+        }
+    }
+    cout << "Error: Línea no encontrada." << endl;
+}
+
+int RedMetro::obtenerCantidadLineas() {
+    return cantidadLineas;
+}
+
+Linea** RedMetro::getLineas() {
+    return lineas;
+}
+
+int RedMetro::getCantidadLineas() {
+    return cantidadLineas;
 }
